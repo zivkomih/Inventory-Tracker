80b2ccbd08a774e43371b8bf5d328e0dd4f22117
zivkomih <85724086+zivkomih@users.noreply.github.com>
2024-05-19 08:14:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,18 @@ data_values_beliefs = {
 }
 
 data_luxury_purchase = {
-    'Category': ['Interest in Luxury Goods', 'Purchase Motive: Quality', 'Purchase Motive: Comfort'],
-    'Percentage': [28, 66, 62]
+    'Category': ['Interest in Luxury Goods', 'Purchase Motive: Quality', 'Purchase Motive: Comfort', 'Purchase Motive: Technology', 'Purchase Motive: Status Symbol'],
+    'Percentage': [28, 66, 62, 50, 45]
 }
 
 data_mobility_preferences = {
-    'Category': ['Use of Public Transport', 'Car Usage', 'Air Travel', 'Bicycle Usage', 'Car Sharing', 'Transport Criteria: Convenience', 'Transport Criteria: Practicality'],
-    'Percentage': [38, 45, 23, 15, 5, 62, 68]
+    'Category': ['Use of Public Transport', 'Car Usage', 'Air Travel', 'Bicycle Usage', 'Car Sharing'],
+    'Percentage': [38, 45, 23, 15, 5]
+}
+
+data_transport_criteria = {
+    'Category': ['Transport Criteria: Convenience', 'Transport Criteria: Practicality'],
+    'Percentage': [62, 68]
 }
 
 data_porsche_spirituality = {
@@ -28,6 +33,7 @@ data_porsche_spirituality = {
 df_values_beliefs = pd.DataFrame(data_values_beliefs).sort_values(by='Percentage', ascending=False)
 df_luxury_purchase = pd.DataFrame(data_luxury_purchase).sort_values(by='Percentage', ascending=False)
 df_mobility_preferences = pd.DataFrame(data_mobility_preferences).sort_values(by='Percentage', ascending=False)
+df_transport_criteria = pd.DataFrame(data_transport_criteria).sort_values(by='Percentage', ascending=False)
 df_porsche_spirituality = pd.DataFrame(data_porsche_spirituality).sort_values(by='Percentage', ascending=False)
 
 # Stil für die Diagramme festlegen
@@ -71,7 +77,19 @@ plt.yticks(fontsize=12)
 plt.grid(axis='x')
 st.pyplot(plt.gcf())
 
-# Plot 4: Connection between Porsche and Spirituality
+# Plot 4: Transport Criteria
+st.subheader('Transport Criteria')
+plt.figure(figsize=(12, 7))
+sns.barplot(x='Percentage', y='Category', data=df_transport_criteria, palette="cubehelix")
+plt.title('Transport Criteria', fontsize=16)
+plt.xlabel('Percentage', fontsize=14)
+plt.ylabel('')
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+plt.grid(axis='x')
+st.pyplot(plt.gcf())
+
+# Plot 5: Connection between Porsche and Spirituality
 st.subheader('Connection between Porsche and Spirituality')
 plt.figure(figsize=(12, 7))
 sns.barplot(x='Percentage', y='Category', data=df_porsche_spirituality, palette="magma")
